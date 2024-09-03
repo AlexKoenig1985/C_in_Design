427fe682f38ab4f3975bf8b7f8bd22a76f23bfd7
AlexKoenig1985 <99577461+AlexKoenig1985@users.noreply.github.com>
2024-09-03 04:27:05 +0000

    Learning material for C programming language

---
+++ MultiplicationTable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int mul = 0;
+    int num = 0;
+
+    printf("Enter a integer: ");
+    scanf("%d", &num);
+    for(int mulFac =1; mulFac <= 10; mulFac++)
+    {
+        mul = num * mulFac;
+        printf("%d*%d: %d \n", num, mulFac, mul);
+    }
+}
